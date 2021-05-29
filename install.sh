08ca70f4f5d0016f30e2bdbd22540db04b8fee05
chunu67 <56378755+chunu67@users.noreply.github.com>
2021-05-29 12:46:00 +0530

    2nd
    Update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+gf
 
 go get github@tomnomnom/kxss
 
@@ -10,3 +11,34 @@ go get -u github.com/tomnomnom/hacks/unisub
 go get -u github.com/tomnomnom/hacks/urinteresting
 
 https://github.com/tomnomnom/hacks.git
+
+qsreplace
+unew
+
+
+echo "subfinder"
+go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+echo "hakrawler"
+go get github.com/hakluke/hakrawler
+
+
+"crlfuzz"
+go get -v github.com/dwisiswant0/crlfuzz/cmd/crlfuzz 
+
+filter-resolved 
+
+echo "interactsh-client"
+go get -v github.com/projectdiscovery/interactsh/cmd/interactsh-client
+
+echo "fuff"
+go get -u github.com/ffuf/ffuf	
+
+echo "gau"
+go get -u -v github.com/lc/gau
+
+
+echo "hakrawler"  
+go get github.com/hakluke/hakrawler
+
+
