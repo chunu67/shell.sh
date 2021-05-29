3a888c795dd58a9a677e2712e0daed57b39ab975
chunu67 <56378755+chunu67@users.noreply.github.com>
2021-05-29 10:19:11 +0530

    Create install.sh
    hello installation

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+go get github@tomnomnom/kxss
+
+go get github@tomnomnom/unfurl
+
+get github.com/tomnomnom/hacks/waybackurls
+
+go get -u github.com/tomnomnom/hacks/unisub
+ 
+go get -u github.com/tomnomnom/hacks/urinteresting
+
+https://github.com/tomnomnom/hacks.git
